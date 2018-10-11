fce033ddd548f3446b1ac35487dbcfe1cc3339e4
Arthur Villar <=>
2018-10-11 00:29:52 -0400

    Create basic views structure

---
+++ resources/views/components/navbar/dropdown.blade.php
@@ -0,0 +1,10 @@
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{$title}}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          @foreach($items as $item)
+          <a class="dropdown-item" href="{{$item['url']}}">{{$item['title']}}</a>
+          @endforeach
+        </div>
+      </li>
---
+++ resources/views/components/navbar/layout.blade.php
@@ -0,0 +1,72 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Metrópolis</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        @include('components.navbar.link', ['title' => 'Quem Somos', 'url' => '#'])
+        @include('components.navbar.link', ['title' => 'O Ambiente', 'url' => '#'])
+        @include('components.navbar.dropdown', ['title' => 'Serviços', 'items' => [
+                ['title' => 'Mesas de Trabalho', 'url' => '#'],
+                ['title' => 'Sala de Reunião', 'url' => '#']
+        ]])
+        @include('components.navbar.link', ['title' => 'Contato', 'url' => '#'])
+
+    </ul>
+    <form>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Área do Cliente</button>
+    </form>
+  </div>
+</nav>
+{{-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+
+            <ul class="navbar-nav ml-auto">
+
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        @if (Route::has('register'))
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        @endif
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav> --}}

720e00edee080dc3a02c7bdee29b48a3e3b0e833
Arthur Villar <=>
2019-01-28 21:41:16 -0500

    Fix plan carousels

--- resources/views/pages/plans/show/sections/carousel/toquio.blade.php
+++ resources/views/pages/plans/show/sections/carousel/toquio.blade.php
@@ -1,10 +1,10 @@
-<div id="carousel-conference" class="carousel slide h-100" data-ride="carousel">
+<div id="carousel-toquio" class="carousel slide h-100" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carousel-conference" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-conference" data-slide-to="1"></li>
-    <li data-target="#carousel-conference" data-slide-to="2"></li>
-    <li data-target="#carousel-conference" data-slide-to="3"></li>
-    <li data-target="#carousel-conference" data-slide-to="4"></li>
+    <li data-target="#carousel-toquio" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-toquio" data-slide-to="1"></li>
+    <li data-target="#carousel-toquio" data-slide-to="2"></li>
+    <li data-target="#carousel-toquio" data-slide-to="3"></li>
+    <li data-target="#carousel-toquio" data-slide-to="4"></li>
   </ol>
   <div class="carousel-inner h-100">
     <div class="carousel-item active w-100 h-100">
@@ -23,11 +23,11 @@
       <div class="bg-align-center w-100 h-100" style="background-image: url({{asset("images/conference/conference-5.jpg")}})"></div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carousel-conference" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carousel-toquio" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carousel-conference" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carousel-toquio" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>

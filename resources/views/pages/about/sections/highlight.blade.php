b31eea3bada843d20d9b8f19f72829480de26577
Arthur Villar <=>
2019-02-20 00:19:48 -0500

    Update mission and vision

--- resources/views/pages/about/sections/highlight.blade.php
+++ resources/views/pages/about/sections/highlight.blade.php
@@ -8,12 +8,12 @@
 
       <div class="col-md-4 my-3">
         <p><strong>Nossa missão</strong></p>
-        <p class="text-muted m-0">Criar um espaço de trabalho que estimula o crescimento do profissional, desenvolvimento de idéias e ao mesmo tempo oferece assistência e resursos para o sucesso coletivo.</p>
+        <p class="text-muted m-0">Criar um espaço de trabalho que estimula o crescimento do profissional, oferecendo assistência e recursos para o sucesso dos seus projetos.</p>
       </div>
 
       <div class="col-md-4 my-3">
         <p><strong>Nossa visão</strong></p>
-        <p class="text-muted m-0">Potencializar os talentos dos nossos membros, associados e parceiros, criando oportunidades para networking e aprendizado de profissionais das mais diversas áreas.</p>
+        <p class="text-muted m-0">Proporcionar oportunidades para networking e aprendizado entre profissionais das mais diversas áreas.</p>
 
     </div>
   </div>

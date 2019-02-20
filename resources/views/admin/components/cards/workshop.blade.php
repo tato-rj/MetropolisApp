4bb5d6af1fdd060dd781de39594e7af96ae602bb
Arthur Villar <=>
2019-02-20 00:28:38 -0500

    Hide workshop attendees number from main page

--- resources/views/admin/components/cards/workshop.blade.php
+++ resources/views/admin/components/cards/workshop.blade.php
@@ -6,16 +6,16 @@
 		<img class="card-img-top rounded-0" src="{{asset($workshop->cover_image_path)}}" alt="">
 	</a>
 	<div class="card-body flex-column d-flex justify-content-between">
-		<div class="mb-2">
+		<div>
 			<a href="{{route('workshops.show', $workshop->slug)}}" class="link-none">
 				<h5 class="card-title mb-1">{{$workshop->name}}</h5>
 			</a>
 			<p class="mb-1"><small>{{$workshop->starts_at->format('d/m/Y')}} às {{$workshop->starts_at->format('H')}} horas</small></p>
 			<p>{{$workshop->headline}}</p>
-			@include('components.workshops.files-count')
-			@include('components.workshops.attendees-count')
 		</div>
 		@if($editable)
+		@include('components.workshops.files-count')
+		@include('components.workshops.attendees-count')
 		<div class="d-flex">
 			<a href="{{route('admin.workshops.details', $workshop->slug)}}" class="btn btn-teal mr-2">Ver detalhes</a>
 			<a href="{{route('admin.workshops.edit', $workshop->slug)}}" class="btn btn-red">Editar</a>

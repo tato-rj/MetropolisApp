107810a86b862e7fa00b441f902009e357e7646e
Arthur Villar <=>
2019-02-22 00:26:03 -0500

    Order workshop attendees by date

--- app/Workshop.php
+++ app/Workshop.php
@@ -26,7 +26,7 @@ class Workshop extends Metropolis
 
     public function attendees()
     {
-    	return $this->belongsToMany(User::class, 'user_workshops', 'workshop_id', 'user_id')->withTimestamps();
+    	return $this->belongsToMany(User::class, 'user_workshops', 'workshop_id', 'user_id')->latest()->withTimestamps();
     }
 
     public function files()

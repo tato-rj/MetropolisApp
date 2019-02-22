a86ba7cda6fa8c249ded273db3c32987f17f23a6
Arthur Villar <=>
2019-02-22 00:24:01 -0500

    Remove workshop alert from schedule view

--- resources/views/pages/user/schedule/sections/calendar.blade.php
+++ resources/views/pages/user/schedule/sections/calendar.blade.php
@@ -1,11 +1,11 @@
 <section class="mb-7 mt-4 container">
   <div class="row">
 
-	<div class="col-10 mx-auto">
+{{-- 	<div class="col-10 mx-auto">
 		@foreach(auth()->user()->upcomingWorkshops as $workshop)
 		@include('components.alerts.workshop')
 		@endforeach
-	</div>
+	</div> --}}
 
   	<div class="col-10 mx-auto mb-4">
   		<div id="calendar-loading" class="text-muted d-flex align-items-start justify-content-center pt-8" style="min-height: 740px">

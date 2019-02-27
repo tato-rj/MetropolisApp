f677f7812c1546068d837e384b7129961e5c49df
Arthur Villar <=>
2019-02-27 00:36:10 -0500

    Change email logo from svg to png

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <a class="navbar-brand" href="/"><img src="{{asset('images/brand/logo.svg')}}" width="90"></a>
+            <a class="navbar-brand" href="/"><img src="{{asset('images/email/logo.png')}}" width="90"></a>
         </a>
     </td>
 </tr>

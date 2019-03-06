c4da0482cc03b33453c56d2d2e6d00ef092d4a50
Arthur Villar <=>
2019-03-06 13:48:55 -0500

    Remove extra padding from partners icons

--- resources/views/pages/welcome/sections/partners.blade.php
+++ resources/views/pages/welcome/sections/partners.blade.php
@@ -5,29 +5,29 @@
   </div>
   <div class="row align-items-end justify-content-center">
 
-    <div class="col-lg-3 col-md-6 col-8 mx-auto">
-      <div class="px-5 py-4 text-center">
+    <div class="col-lg-3 col-6 mx-auto">
+      <div class="p-4 text-center">
         <a href="" class="link-none">
           <img src="{{asset('images/company1.svg')}}" class="w-100 hover-grayscale-out" style="max-width: 120px">
         </a>
       </div>
     </div>
-    <div class="col-lg-3 col-md-6 col-8 mx-auto">
-      <div class="px-5 py-4 text-center">
+    <div class="col-lg-3 col-6 mx-auto">
+      <div class="p-4 text-center">
         <a href="" class="link-none">
           <img src="{{asset('images/company2.svg')}}" class="w-100 hover-grayscale-out" style="max-width: 120px">
         </a>
       </div>
     </div>
-    <div class="col-lg-3 col-md-6 col-8 mx-auto">
-      <div class="px-5 py-4 text-center">
+    <div class="col-lg-3 col-6 mx-auto">
+      <div class="p-4 text-center">
         <a href="" class="link-none">
           <img src="{{asset('images/company3.svg')}}" class="w-100 hover-grayscale-out" style="max-width: 120px">
         </a>
       </div>
     </div>
-    <div class="col-lg-3 col-md-6 col-8 mx-auto">
-      <div class="px-5 py-4 text-center">
+    <div class="col-lg-3 col-6 mx-auto">
+      <div class="p-4 text-center">
         <a href="" class="link-none">
           <img src="{{asset('images/company4.svg')}}" class="w-100 hover-grayscale-out" style="max-width: 120px">
         </a>

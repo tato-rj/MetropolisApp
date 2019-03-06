208c74a6d4a2b15755b1b479a90cb85c12d8c5a1
Arthur Villar <=>
2019-03-06 11:39:14 -0500

    Change user dashboard button sizes

--- resources/views/pages/user/home/sections/button.blade.php
+++ resources/views/pages/user/home/sections/button.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-6 col-md-6 col-sm-6 col-12 p-3">
+<div class="col-lg-6 col-md-6 col-sm-6 col-12 p-2">
 	<a href="{{$url ?? null}}" class="link-none">
 		<div class="btn-light border-left border-2x border-red p-4 text-dark">
 			<i class="fas text-red fa-{{$icon}} mr-2"></i>

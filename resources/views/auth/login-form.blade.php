d893b609d56115e77726888c06f61fe406f7c2aa
Arthur Villar <=>
2019-03-07 01:50:03 -0500

    Remove social login options

--- resources/views/auth/login-form.blade.php
+++ resources/views/auth/login-form.blade.php
@@ -23,11 +23,11 @@
 
         <button type="submit" class="btn btn-red btn-block">Continuar</button>
     </form>
-    <div class="mb-4">
+{{--     <div class="mb-4">
         <p class="lead text-muted text-center my-3">- ou se preferir -</p>
         <a href="" class="btn btn-facebook btn-block text-left"><i class="fab fa-facebook fa-lg border-right mr-3" style="width: 34px"></i>Continuar com Facebook</a>
         <a href="" class="btn btn-google btn-block text-left"><i class="fab fa-google border-right mr-3" style="width: 34px"></i>Continuar com Google</a>
-    </div>
+    </div> --}}
     <div>
         <p class="mb-0">
             <small>Esqueceu o seu password? <span class="text-blue"><a href="{{route('password.request')}}" class="link-no-blue">Clique aqui</a></span></small>
--- resources/views/auth/register-form.blade.php
+++ resources/views/auth/register-form.blade.php
@@ -32,11 +32,11 @@
 
         <button type="submit" class="btn btn-red btn-block">Entrar</button>
     </form>
-    <div class="mb-4">
+{{--     <div class="mb-4">
         <p class="lead text-muted text-center my-3">- ou se preferir -</p>
         <a href="" class="btn btn-facebook btn-block text-left"><i class="fab fa-facebook fa-lg border-right mr-3" style="width: 34px"></i>Entrar com Facebook</a>
         <a href="" class="btn btn-google btn-block text-left"><i class="fab fa-google border-right mr-3" style="width: 34px"></i>Entrar com Google</a>
-    </div>
+    </div> --}}
     <div>
         <p class="m-0">
             <small>Já possui cadastro? <span class="text-blue"><a href="{{route('login')}}" class="link-no-blue">Fazer o login</a></span></small>

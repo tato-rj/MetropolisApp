70712607751721c9a61091d048475722eeb8347d
Arthur Villar <=>
2019-05-10 14:06:17 -0400

    Implement status verification

--- resources/views/components/modals/results/standalone.blade.php
+++ resources/views/components/modals/results/standalone.blade.php
@@ -83,7 +83,7 @@
 		<span class="text-teal mr-1"><strong>Status</strong></span>
 		
 		<span class="status-label text-{{$event->statusColor}}">{{$event->status}}
-		@admin($event->creator_type)
+		@admin($user_type)
 		@if($event->status == 'Processando o pedido')
 		<button class="btn btn-sm btn-link align-text-top p-0 ml-2 event-status" data-code="{{$event->reference}}" data-url="{{route('pagseguro.event.status')}}"><i class="fas fa-sync-alt mr-1"></i>atualizar</button>
 		@endif

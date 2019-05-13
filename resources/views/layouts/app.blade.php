f15bbf6cb718a3b79eff1bc1d7f11f41daeba42b
Arthur Villar <=>
2019-05-13 08:33:38 -0400

    Install google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,16 @@
         ]); ?>
     </script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140027196-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-140027196-2');
+    </script>
+
     @stack('header')
 
 </head>

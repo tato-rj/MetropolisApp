64da786eb87dccea161116c4969a156427798c53
Arthur Villar <=>
2019-05-13 09:40:08 -0400

    Update membership verification

--- resources/views/emails/plan/confirm.blade.php
+++ resources/views/emails/plan/confirm.blade.php
@@ -2,6 +2,7 @@
 # Olá {{$user->first_name}}
 
 <p>Você realizou com sucesso a assinatura do <span class="text-uppercase text-{{$user->membership->plan->color}}"><strong>{{$user->membership->plan->displayName}}</strong></span>. O seu plano se renovará automaticamente ao final de cada ciclo. Você pode cancelar essa renovação a qualquer momento, basta acessar o seu Painel de Controle/Pagamentos.</p>
+<p>Por favor aguarde a confirmação do seu pagamento para finalizarmos a reserva.</p>
 <p>O seu plano oferece <strong>
 	{{$user->membership->plan->bonus_limit}} 
 	{{trans_choice('words.horas', $user->membership->plan->bonus_limit)}} 
